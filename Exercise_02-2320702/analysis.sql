b85f9a51f96c310522f0f4a60379e1f4a788b8a2
shiodr <130903072+shiodr@users.noreply.github.com>
2025-04-29 13:37:48 +0800

    Update analysis.sql
    Signed-off-by: shiodr <130903072+shiodr@users.noreply.github.com>

--- Exercise_02-2320702/analysis.sql
+++ Exercise_02-2320702/analysis.sql
@@ -1,4 +1,4 @@
-SELECT vehicle_type, SUM(killed) AS total_killed
+SELECT vehicle_type, SUM(killed) AS total_killed  /* With the highest killed from a vehicle type */
 FROM (
        SELECT 
         VEHICLE_TYPE_CODE_1 AS vehicle_type, 
@@ -31,7 +31,7 @@ GROUP BY vehicle_type
 ORDER BY total_killed DESC
 LIMIT 100;
 
-SELECT
+SELECT /* With the highest killed from a location with year */
     BOROUGH AS location,
     YEAR(STR_TO_DATE(CRASH_DATE, '%m/%d/%Y')) AS crash_year,
     SUM(CAST(NUMBER_OF_PERSONS_KILLED AS UNSIGNED)) AS total_killed
@@ -41,7 +41,7 @@ GROUP BY BOROUGH, crash_year
 ORDER BY total_killed DESC
 LIMIT 100;
 
-SELECT
+SELECT /* With the highest killed from Year*/
     YEAR(STR_TO_DATE(CRASH_DATE, '%m/%d/%Y')) AS crash_year,
     SUM(CAST(NUMBER_OF_PERSONS_KILLED AS UNSIGNED)) AS total_killed
 FROM exercise_02.vehicle_collision
