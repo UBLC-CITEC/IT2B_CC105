da9e98d049c2a31876b1899f121da4a6a7270675
Gzushiii <142777939+Gzushiii@users.noreply.github.com>
2025-04-29 13:46:39 +0800

    Update and rename Street_Name.sql to Analysis.sql
    Analysis of exercise_02.
    
    Signed-off-by: Gzushiii <142777939+Gzushiii@users.noreply.github.com>

--- Exercise_02-2320410/Street_Name.sql
+++
@@ -1,5 +0,0 @@
-SELECT COUNT(*) FROM exercise_02.vehicle_collision;
-
-SELECT * FROM exercise_02.vehicle_collision limit 100;
-
-SELECT DISTINCT(ON_STREET_NAME), COUNT(*) as COUNT FROM exercise_02.vehicle_collision GROUP BY NUMBER_OF_PERSONS_KILLED asc;
